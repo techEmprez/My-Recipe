90b7af8e071221afdf0c3696ca2620130b0372e3
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-05-27 13:33:15 +0200

    add tests for the foods and recipes

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.feature 'Foods', type: :feature do
+  before(:each) do
+    @food = Food.create(name: 'Apple', measurement_unit: 'kg', quantity: 5, price: 2.5)
+  end
+
+  scenario 'User views the list of foods' do
+    visit foods_path
+
+    expect(page).to have_content('Apple')
+    expect(page).to have_content('kg')
+    expect(page).to have_content('5')
+    expect(page).to have_content('$2.5')
+  end
+
+  scenario 'User views a specific food' do
+    visit food_path(@food)
+
+    expect(page).to have_content('Apple')
+    expect(page).to have_content('kg')
+    expect(page).to have_content('5')
+    expect(page).to have_content('$2.5')
+  end
+
+  scenario 'User deletes a food' do
+    visit foods_path
+
+    expect { click_button 'Delete' }.to change(Food, :count).by(-1)
+    expect(page).not_to have_content('Apple')
+  end
 end
--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe RecipeFoodsController, type: :controller do
+  describe 'GET #index' do
+    it 'assigns all recipe foods to @recipe_foods' do
+      recipe_food1 = create(:recipe_food)
+      recipe_food2 = create(:recipe_food)
+
+      get :index
+
+      expect(assigns(:recipe_foods)).to match_array([recipe_food1, recipe_food2])
+    end
+
+    it 'renders the index template' do
+      get :index
+
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid parameters' do
+      let(:valid_params) { attributes_for(:recipe_food) }
+
+      it 'creates a new recipe food' do
+        expect do
+          post :create, params: { recipe_food: valid_params }
+        end.to change(RecipeFood, :count).by(1)
+      end
+
+      it 'redirects to the created recipe food' do
+        post :create, params: { recipe_food: valid_params }
+
+        expect(response).to redirect_to(recipe_food_url(RecipeFood.last))
+      end
+    end
+
+    context 'with invalid parameters' do
+      let(:invalid_params) { attributes_for(:recipe_food, quantity: nil) }
+
+      it 'does not create a new recipe food' do
+        expect do
+          post :create, params: { recipe_food: invalid_params }
+        end.to_not change(RecipeFood, :count)
+      end
+
+      it 'renders the new template' do
+        post :create, params: { recipe_food: invalid_params }
+
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
